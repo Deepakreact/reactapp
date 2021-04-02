325d40ba3471a5072e355176ba732de784bb2a3d
Deepakreact <deepaksharmaosho@gmail.com>
2021-04-02 18:28:05 +0530

    automataic slider with fade effect

--- src/Componants/Slider.js
+++ src/Componants/Slider.js
@@ -1,11 +1,25 @@
-import React,{useState} from 'react'
+import React,{useState,useRef,useEffect} from 'react'
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import Fade from 'react-reveal/Fade'
 import './slider.css'
 
 function Slider({slider}) {
     const [current, setCurrent] = useState(0)
     const length=slider.length
+    const timeout=useRef(null)
+
+    useEffect(() => {
+        const nextSlide=()=>{
+            setCurrent(current===length-1 ? 0 :current + 1)
+        }
+
+        timeout.current= setTimeout(nextSlide,5000)
+        
+        return () => {
+            //
+        }
+    }, [current,length])
     
     const preSlide=()=>{
         setCurrent( current===0 ? length-1 : current-1)
@@ -23,23 +37,38 @@ function Slider({slider}) {
     return (
         <div>
         <div className="slider">
+
         {
             slider.map((slide,index)=>
             
             
             <div className="slides" key={index}>
+      
+        
             {
                 index===current &&(
+              
+
             <div className="slide">
+            <Fade>
+  
+      
             <img  src={slide.image} alt={slide._id}></img>
+            </Fade>
+       
+         
             </div>
+
             
             )
             }
+      
+     
             </div>
             
             )
         }
+    
 
         
         
